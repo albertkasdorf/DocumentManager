e7cc2ecc81fdab07b00692e796250023512e0423
Albert Kasdorf <albert.kasdorf@live.de>
2017-03-12 21:38:46 +0100

    Initial Visual Studio Project commit.

---
+++ DocumentManager/Program.cs
@@ -0,0 +1,17 @@
+﻿using DocumentManager.UI;
+using System;
+using System.Windows.Forms;
+
+namespace DocumentManager
+{
+	public static class Program
+	{
+		[STAThread]
+		public static void Main( )
+		{
+			Application.EnableVisualStyles( );
+			Application.SetCompatibleTextRenderingDefault( false );
+			Application.Run( new MainForm( ) );
+		}
+	}
+}
