43574840fa56b7ec0ce84a4c891ebe4078528258
Albert Kasdorf <albert.kasdorf@live.de>
2017-03-14 21:33:11 +0100

    FileNameExtractor extract the information field of an provided filename.

---
+++ DocumentManager/Logic/FileNameExtractor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManager.Logic
+{
+	public class FileNameExtractor
+	{
+		public FileNameExtractor( String separator = " - " )
+		{
+			this.separator = new String[] { separator };
+		}
+
+		private String[] separator;
+
+		public Tuple<DateTime, String, String> Extract( String fileName )
+		{
+			var fnwe = Path.GetFileNameWithoutExtension( fileName );
+			var splits = fnwe.Split( separator, 3, StringSplitOptions.None );
+
+			if( splits.Length < 3 )
+				throw new FormatException( "Field count is small than three." );
+
+			var existEmptyField = splits.Any( x => string.IsNullOrEmpty( x ) );
+			if( existEmptyField )
+				throw new FormatException( "Empty field found." );
+
+			var date = splits[0].Trim( );
+			var dateTime = DateTime.ParseExact(
+				date, "yyyy-MM-dd", CultureInfo.InvariantCulture );
+			var organisation = splits[1].Trim( );
+			var title = splits[2].Trim( );
+
+			var result = Tuple.Create( dateTime, organisation, title );
+
+			return result;
+		}
+	}
+}
---
+++ DocumentManagerTests/FileNameExtractorTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentManager.Logic;
+
+namespace DocumentManager.Tests
+{
+	[TestClass]
+	public class FileNameExtractorTest
+	{
+		[TestMethod]
+		public void CorrectFileNameTest( )
+		{
+			var fileName =
+				"2017-03-14 - FH Aachen - Bescheinigung über angemeldete Prüfungen";
+			var expected = Tuple.Create(
+				new DateTime( 2017, 03, 14 ),
+				"FH Aachen", "Bescheinigung über angemeldete Prüfungen" );
+
+			var fne = new FileNameExtractor( );
+			var actual = fne.Extract( fileName );
+
+			Assert.AreEqual( expected, actual );
+		}
+
+		[TestMethod]
+		[ExpectedException( typeof( FormatException ) )]
+		public void FileNameWithoutSeparatorTest( )
+		{
+			var fileName = "2017-01-05-Cineplex-Vaiana.pdf";
+			var expected = Tuple.Create(
+				new DateTime( 2017, 01, 05 ), "Cineplex", "Vaiana" );
+
+			var fne = new FileNameExtractor( );
+			var actual = fne.Extract( fileName );
+
+			Assert.AreEqual( expected, actual );
+		}
+
+		[TestMethod]
+		public void FileNameWithManySeparatorTest( )
+		{
+			var fileName =
+				"2017-01-02 - Sparkasse - Konto_123456789 - Auszug_2110_012.PDF";
+			var expected = Tuple.Create(
+				new DateTime( 2017, 01, 02 ),
+				"Sparkasse", "Konto_123456789 - Auszug_2110_012" );
+
+			var fne = new FileNameExtractor( );
+			var actual = fne.Extract( fileName );
+
+			Assert.AreEqual( expected, actual );
+		}
+
+		[TestMethod]
+		public void FileNameWithManyWhitespacesTest( )
+		{
+			var fileName =
+				"   2016-10-29      -   Steam   -    Pillars of Eternity    .pdf";
+			var expected = Tuple.Create(
+				new DateTime( 2016, 10, 29 ),
+				"Steam", "Pillars of Eternity" );
+
+			var fne = new FileNameExtractor( );
+			var actual = fne.Extract( fileName );
+
+			Assert.AreEqual( expected, actual );
+		}
+
+		[TestMethod]
+		[ExpectedException( typeof( FormatException ) )]
+		public void FileNameWithWrongDateFormatTest( )
+		{
+			var fileName =
+				"22-10-2016 - Bonding - Hackathon.pdf";
+			var expected = Tuple.Create(
+				new DateTime( 2016, 10, 22 ),
+				"Bonding", "Hackathon" );
+
+			var fne = new FileNameExtractor( );
+			var actual = fne.Extract( fileName );
+
+			Assert.AreEqual( expected, actual );
+		}
+
+		[TestMethod]
+		[ExpectedException( typeof( FormatException ) )]
+		public void FileNameWithEmptyDateFieldTest( )
+		{
+			var fileName = " - Steam - Pillars of Eternity.pdf";
+			var fne = new FileNameExtractor( );
+
+			fne.Extract( fileName );
+		}
+
+		[TestMethod]
+		[ExpectedException( typeof( FormatException ) )]
+		public void FileNameWithEmptyOrganisationFieldTest( )
+		{
+			var fileName = "2016-10-29 -  - Pillars of Eternity.pdf";
+			var fne = new FileNameExtractor( );
+
+			fne.Extract( fileName );
+		}
+
+		[TestMethod]
+		[ExpectedException( typeof( FormatException ) )]
+		public void FileNameWithEmptyTitleFieldTest( )
+		{
+			var fileName = "2016-10-29 - Steam - .pdf";
+			var fne = new FileNameExtractor( );
+
+			fne.Extract( fileName );
+		}
+	}
+}
